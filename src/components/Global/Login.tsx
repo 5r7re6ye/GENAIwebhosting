44052849950fc01b96e641f15fda47fa86c6d3aa
Ivan Kong <ivankong9656@gmail.com>
2025-10-25 03:41:13 +0800

    Fix background image imports using proper Vite asset handling

--- src/components/Global/Login.tsx
+++ src/components/Global/Login.tsx
@@ -3,6 +3,7 @@ import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { auth, db } from "../../firebase/config";
 import GHeader from "./Header";
+import backgroundImage from "../../assets/Homebackground.jpg";
 
 interface LoginProps {
   onLoginSuccess: (userType: string) => void;
@@ -135,7 +136,7 @@ function Login({ onLoginSuccess, onSignUpClick }: LoginProps) {
     <div
       style={{
         backgroundImage:
-          "linear-gradient(rgba(255,255,255,0.6), rgba(255,255,255,0.6)), url('/GENAI-webhosting/Homebackground.jpg')",
+          `linear-gradient(rgba(255,255,255,0.6), rgba(255,255,255,0.6)), url(${backgroundImage})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
